8040025486e4a9ee61909196dc8c6ccf87a41d2a
Madhav Javia <madhav@Madhavs-MacBook-Pro.local>
2025-06-23 22:14:09 +0530

    23June

--- 23JUNE/3.py
+++ 23JUNE/3.py
@@ -1,17 +1,29 @@
-a=[1,3,2,'Hzllo','Hello','hey']
+
+a=eval(input("Enter a list: "))
+# a=[1,3,2,'Hzllo','Hello','hey']
 b=[]
 c=[]
 
+def bubbleSort(list):
+    n = len(list)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
 
 for i in a:
     if isinstance(i, int):
         print(f"{i} is a digit")
         b.append(i)
-    elif isinstance(i, str) and i.isalpha():
+    if isinstance(i, str):
         print(f"{i} is an alphabet")
         c.append(i)
 
-b.sort()
-c.sort()
+# b.sort()
+# c.sort()
+bubbleSort(b)
+bubbleSort(c)
+# c.sort()
 b.extend(c)
 print(b)
+print(c)
